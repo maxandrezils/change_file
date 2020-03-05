4b5acc8ec7ba157559c4f11cd4ab03ea5c2813b1
Max-Andre <zilsmc@gmail.com>
2020-03-05 08:05:39 +0200

    added comments to copy_file

--- copy_file.py
+++ copy_file.py
@@ -1,17 +1,17 @@
-import os
-import re
-import shutil
-from os import path
+import os           # to access the operating system
+import re           # to perform a regex check on the name of the file
+import shutil       # library to copy files
+from os import path # access the file path i.e C:\Users\
 
 def main(original):
     if path.exists(original):
-        fileSplit = re.findall(r'\d+', original)
+        fileSplit = re.findall(r'\d+', original) # d+ looks for all decimals
         newDate = int(fileSplit[0]) + int(1)
-        fileName = re.findall(r'(\w+ )', original)
+        fileName = re.findall(r'(\w+ )', original)# w+space looks for all words plus spaces
         shutil.copyfile(original, ''.join(fileName)+str(newDate)+".xlsx")
         print(''.join(fileName)+str(newDate)+".xlsx" + " has been duplicated from " + original)
     else:
         print("File does not exist")
 
 
-main("guru new example 202002.xlsx")
+main("demo file 202002.xlsx")
